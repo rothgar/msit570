d894b3c50fb4d1264cb88331a694f9c6259efbde
Justin Garrison <justinleegarrison@gmail.com>
2014-11-22 00:26:21 -0800

    [WIP]Initial for loop comit

---
+++ ChineseZodiac/Chinese_Zodiac_for_loop.php
@@ -0,0 +1,17 @@
+<?php
+$Zsigns = array( "rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "sheep", "monkey", "rooster", "dog", "pig");
+<table style="width:100%">
+    <tr>
+    for ( $sign = 0; $sign < count($sign); ++$sign) {
+        <td>images/$Zsigns[$sign].png</td>;
+    }
+    </tr>
+    <tr>
+    for ( $year = 1912; $year <= data("Y"); ++$year ) {
+        $col = 12;
+        <td>$year</td>;
+        $col++;
+    }
+    </tr>
+</table>
+?>
