fe11bbfe54d8cfa5754f096934e990e34849d661
Justin Garrison <justinleegarrison@gmail.com>
2014-11-21 23:17:55 -0800

    added initial index.php and style.css

---
+++ ChineseZodiac/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<title>Chinese Zodiac</title>
+</head>
+
+<body>
+
+<div id="header">
+include php_include/inc_header.php
+</div>
+
+<div id="txtnav">
+include php_include/inc_text_links.php
+</div>
+
+<div id="btnnav">
+include php_include/inc_button_nav.php
+</div>
+
+</div>
+include php_include/inc_home.php
+</div>
+
+<div id="footer">
+include php_include/inc_footer.php
+</div>
+
+</body>
+
+</html>
