cbd1326109b96671ec9efde2234f4638de00e342
Justin Garrison <justinleegarrison@gmail.com>
2014-11-27 23:59:31 -0800

    Finished TempConversion script

--- Ex2.6/TempConversion.php
+++ Ex2.6/TempConversion.php
@@ -2,18 +2,42 @@
 <html>
 <head></head>
 <body>
-<table border="1">
-<tr>
-    <td>Fahrenheit</td>
-    <td>Celsius</td>
+
+<!-- main form -->
+<form name="degree" method="GET" action="#">
+<!-- do some radio button magic to make the last selection stick -->
+fahrenheit to celcius <input type="radio" name="scale" value="f" <?php if(!isset($_GET['scale']) || (isset($_GET['scale']) && $_GET['scale'] == 'f')) echo ' checked="checked"';?>>
+    <br>
+    celcius to fahrenheit <input type="radio" name="scale" value="c" <?php if(isset($_GET['scale']) && $_GET['scale'] == 'c')  echo ' checked="checked"';?>>
+    <br>
+    <br>
+    <!-- Temperature we are going to convert -->
+    <input type="text" name="degrees" value="<?php print $sub_degrees ; ?>"> Degrees
+    <br>
+    <br>
+    <input type="submit" name="temp_submit" value="Convert">
+</form>
+
 <?php
-$i = 0;
-while ($i < 101 ) {
-    echo "<tr>";
-    $j=round(($i-32)*(5/9), 1);
-    echo "<td>$i</td>.<td>$j</td>";
-    $i++;
-    echo "</tr>";
+// first check if the button has been pressed
+if (isset($_GET['temp_submit'])) {
+    // if scale is selected, set the variable
+    if (isset($_GET['scale'])) {
+        $scale = $_GET['scale'];
+    } else {
+        // this should never happen, but just in case
+        exit ('No scale selected' );
+    }
+    echo "<br>";
+    // get submitted degrees
+    $sub_degrees = $_GET['degrees'];
+    if ( $scale == "f" ) {
+        // echo conversion for f -> c
+        echo round(($sub_degrees-32)*(5/9), 1);
+    } else {
+        // conversion for c -> f
+        echo round(($sub_degrees*(9/5))+32, 1);
+    }
 }
 ?>
 </table>
