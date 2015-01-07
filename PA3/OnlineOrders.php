6495dc7bb41cfd61796da0efd7eaa40b2f767769
Justin Garrison <justinleegarrison@gmail.com>
2015-01-06 22:57:08 -0800

    Initial comit for online orders

---
+++ PA3/OnlineOrders.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+
+<?php
+// set item variables
+// item,avail,cost,desc
+$items = array
+  (
+    array(
+      "name" => "Sony PS4",
+      "avail" => "50",
+      "cost" => "400",
+      "desc" => "Next generation gaming console from Sony"
+    ),
+    array(
+      "name" => "Microsoft Xbox One",
+      "avail" => "500",
+      "cost" => "500",
+      "desc" => "Next generation gaming console from Microsoft"
+    ),
+    array(
+      "name" => "Nintendo WiiU",
+      "avail" => "309",
+      "cost" => "200",
+      "desc" => "Last generation gaming console from Nintendo"
+    ),
+    array(
+      "name" => "Playstation Vita",
+      "avail" => "240",
+      "cost" => "200",
+      "desc" => "Handheld gaming system from Sony"
+    ),
+    array(
+      "name" => "Nintendo 3DS",
+      "avail" => "50",
+      "cost" => "150",
+      "desc" => "Handheld gaming system from Nintendo"
+    ),
+    array(
+      "name" => "Steam Machine",
+      "avail" => "5",
+      "cost" => "600",
+      "desc" => "Video game console from Valve"
+    ),
+    array(
+      "name" => "Apple iPad",
+      "avail" => "500",
+      "cost" => "500",
+      "desc" => "Tablet from Apple"
+    )
+  );
+?>
+
+<form name=orders method="POST" action="#">
+<table border=1> 
+<?php
+// print items
+for ($i = 0; $i < count($items); $i++) {
+  echo "<tr>";
+  echo '<td> <input type=number min=0 step=1 name="'.strtolower(preg_replace("/[\s]/", "-", $items[$i][name])). '_qty" value="'. strtolower(preg_replace("/[\s]/", "-", $items[$i][name])).'_qty>" </td>';
+ // echo strtolower(preg_replace("/[\s]/", "-", $items[$i][name]));
+  echo "<td>".$items[$i][name]."</td>";
+  echo "<td>".$items[$i][avail]."</td>";
+  echo "<td>".$items[$i][desc]."</td>";
+  echo "<td>".$items[$i][cost]."</td>";
+  echo "</tr>";
+}
+?>
+</table>
+<input type="submit" name="update_order" value="Update Order">
+<input type="submit" name="submit_order" value="Place Order">
+
+</body>
+</html>
+
