fa914abb3fb2f680a4af15911e39443ac4a04b23
Justin Garrison <justinleegarrison@gmail.com>
2015-01-13 23:39:39 -0800

    working for loops

--- ChineseZodiac/Chinese_Zodiac_for_loop.php
+++ ChineseZodiac/Chinese_Zodiac_for_loop.php
@@ -1,17 +1,42 @@
+<style>
+table {
+  border: 1px solid black;
+  width: 100%;
+}
+td {
+  border: 1px solid black;
+}
+</style>
+
 <?php
-$Zsigns = array( "rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "sheep", "monkey", "rooster", "dog", "pig");
-echo '<table style="width:100%">';
-    echo '<tr>';
-    for ( $sign = 0; $sign < count($Zsigns); ++$sign) {
-        echo '<td><img src="images/'.$Zsigns[$sign].'.png"></td>';
-    }
+/* Justin Garrison
+ * 2015-01-
+ * 
+ */
+
+// create zodiac signs array
+$signs = array( "rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "sheep", "monkey", "rooster", "dog", "pig");
+// set timezone to get current year
+date_default_timezone_set('America/Los_Angeles');
+// initialize variable to count columns
+$col = 1;
+
+echo '<table>';
+echo '<tr>';
+// header row for images
+for ( $sign = 0; $sign < count($signs); ++$sign) {
+  echo '<td><img src="images/'.$signs[$sign].'.png"></td>';
+}
+echo '</tr>';
+echo '<tr>';
+for ( $year = 1912; $year <= date("Y"); ++$year ) {
+  echo '<td>'.$year.'</td>';
+  if ( $col % 12 === 0 ) {
     echo '</tr>';
     echo '<tr>';
-    for ( $year = 1912; $year <= data("Y"); ++$year ) {
-        $col = 12;
-        echo '<td>'.$year.'</td>';
-        $col++;
-    }
-    echo '</tr>';
+  }
+  $col++;
+}
+echo '</tr>';
 echo '</table>';
 ?>
