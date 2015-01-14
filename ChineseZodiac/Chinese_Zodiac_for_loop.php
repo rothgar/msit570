e611d31b86bea5c600ddbfd1c8c552381ad40ded
Justin Garrison <justinleegarrison@gmail.com>
2015-01-13 22:21:26 -0800

    updating for loop (missing quotes)

--- ChineseZodiac/Chinese_Zodiac_for_loop.php
+++ ChineseZodiac/Chinese_Zodiac_for_loop.php
@@ -1,17 +1,17 @@
 <?php
 $Zsigns = array( "rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "sheep", "monkey", "rooster", "dog", "pig");
-<table style="width:100%">
-    <tr>
-    for ( $sign = 0; $sign < count($sign); ++$sign) {
-        <td>images/$Zsigns[$sign].png</td>;
+echo '<table style="width:100%">';
+    echo '<tr>';
+    for ( $sign = 0; $sign < count($Zsigns); ++$sign) {
+        echo '<td><img src="images/'.$Zsigns[$sign].'.png"></td>';
     }
-    </tr>
-    <tr>
+    echo '</tr>';
+    echo '<tr>';
     for ( $year = 1912; $year <= data("Y"); ++$year ) {
         $col = 12;
-        <td>$year</td>;
+        echo '<td>'.$year.'</td>';
         $col++;
     }
-    </tr>
-</table>
+    echo '</tr>';
+echo '</table>';
 ?>
