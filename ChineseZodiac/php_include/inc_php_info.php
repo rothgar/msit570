973e8070a513c3baed173679966d75a8f10b2560
Justin Garrison <justinleegarrison@gmail.com>
2015-01-15 23:16:17 -0800

    discovery project 3-2

---
+++ ChineseZodiac/php_include/inc_php_info.php
@@ -0,0 +1,10 @@
+<?php
+include 'home_links_bar.inc'
+?>
+
+<p>
+PHP is a web development language that allows development of advanced webpages
+with dynamic content. With PHP you can more easily work with sessions and content
+stored in databases and program reactive content with many more advanced features
+than HTML.
+</p>
