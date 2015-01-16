12b02169fdfa0b029fdf2ccf8e0f321fca06f73f
Justin Garrison <justinleegarrison@gmail.com>
2015-01-15 22:27:38 -0800

    discovery project 3-1

---
+++ ChineseZodiac/php_include/inc_chinese_zodiac.php
@@ -0,0 +1,38 @@
+<p>The Chinese zodiac, Shēngxiào, literally meaning "birth likeness", is a
+cyclical scheme to provide meaning and predictability to personalities and
+human behavior.(1) The system is similar to western astrology in predicting
+behavior patterns and providing broad guidance for parents and friends. The
+Chinese zodiac takes its characteristic naming from 12 different animals. The
+animals attempt to predict how a person will act and what traits they will
+have. The animals of the Chinese zodiac are:</p>
+
+<ul>
+ <li>rwat</li>
+ <li>ox</li>
+ <li>tiger</li>
+ <li>rabbit</li>
+ <li>dragon</li>
+ <li>snake</li>
+ <li>horse</li>
+ <li>goat</li>
+ <li>monkey</li>
+ <li>rooster</li>
+ <li>dog</li>
+ <li>pig</li>
+</ul>
+
+<p>
+  Each animal has special defining characteristics and is assigned to a person
+  based on when they were born in the Chinese calendar.
+</p>
+
+<p>
+  The Chinese zodiac not only applies to years but can also be applied to days,
+  months and times of the day to narrow down someones sign. As in Western
+  Astrology, persons sign is supposed to show how they will behave in certain
+  situations and also help them find compatible people for friendship and
+  companionship based on their signs.
+</p>
+
+(1) http://en.wikipedia.org/wiki/Chinese_zodiac
+
