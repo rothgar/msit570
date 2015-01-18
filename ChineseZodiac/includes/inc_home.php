5ab5c50898e27abcaf91340a01907e4709a12709
Justin Garrison <justinleegarrison@gmail.com>
2015-01-17 23:14:13 -0800

    discovery project 4-5

---
+++ ChineseZodiac/includes/inc_home.php
@@ -0,0 +1,15 @@
+<?php
+if (isset($_GET['section'])) {
+  switch ($_GET['section']) {
+    case 'zodiac':
+      include('includes/inc_chinese_zodiac.php');
+      break;
+    case 'php': // A value of 'php' means to display the default page
+    default:
+      include('includes/inc_php_info.php');
+      break;
+   }
+}
+else // If no section has been selected, then display the default page
+  include('includes/inc_php_info.php');
+?>
