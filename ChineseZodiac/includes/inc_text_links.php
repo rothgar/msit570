bf8a85e6e86a91d59a0082ff1c0ce4240466744f
Justin Garrison <justinleegarrison@gmail.com>
2015-01-17 22:40:30 -0800

    discovery project 4-3

---
+++ ChineseZodiac/includes/inc_text_links.php
@@ -0,0 +1,10 @@
+<a href = "index.php?page=home_page">Home Page</a>
+<a href = "index.php?page=site_layout">Site Layout</a>
+<a href = "index.php?page=control_structures">Control Structures</a>
+<a href = "index.php?page=string_functions">String Functions</a>
+<a href = "index.php?page=web_forms">Web Forms</a>
+<a href = "index.php?page=midterm_assessment">Midterm Assessment</a>
+<a href = "index.php?page=state_information">State Information</a>
+<a href = "index.php?page=user_templates">User Templates</a>
+<a href = "index.php?page=final_project">Final Project</a>
+
