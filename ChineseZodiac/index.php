a0ca23246dcbb165e35366d8b09cc3a4bcd1b717
Justin Garrison <justinleegarrison@gmail.com>
2015-01-22 23:12:18 -0800

    Removed unwanted <p>s

--- ChineseZodiac/index.php
+++ ChineseZodiac/index.php
@@ -9,62 +9,63 @@
 
 <body>
 
-<div id="header">
-<?php include_once(__DIR__.'/include/inc_header.php') ?>
-</div>
+<div id="wrapper">
+  <div id="header">
+    <?php include_once(__DIR__.'/include/inc_header.php') ?>
+  </div>
 
-<div id="txtnav">
-<?php include_once(__DIR__.'/include/inc_text_links.php') ?>
-</div>
+  <div id="txtnav">
+    <p><?php include_once(__DIR__.'/include/inc_text_links.php') ?></p>
+  </div>
 
-<div id="btnnav">
-<?php include_once(__DIR__.'/include/inc_button_nav.php') ?>
-</div>
+  <div id="btnnav">
+    <p><?php include_once(__DIR__.'/include/inc_button_nav.php') ?></p>
+  </div>
 
-<div id="section">
-<?php
-if (isset($_GET['page'])) {
-  switch ($_GET['page']) {
-    case 'site_layout':
-      include('include/' . 'inc_site_layout.php');
-      break;
-    case 'control_structures':
-      include('include/' . 'inc_control_structures.php');
-      break;
-    case 'string_functions':
-      include('include/' . 'inc_string_functions.php');
-      break;
-    case 'web_forms':
-      include('include/' . 'inc_web_forms.php');
-      break; 
-    case 'midterm_assessment':
-      include('include/' . 'inc_midterm_assessment.php');
-      break;
-    case 'state_information':
-      include('include/' . 'inc_state_information.php');
-      break;
-    case 'user_templates':
-      include('include/' . 'inc_user_templates.php');
-      break;
-    case 'final_project':
-      include('include/' . 'inc_final_project.php');
-      break;
-    case 'home_page': // A value of 'home_page' means to display the default page
-    default:
+  <div id="section">
+    <p><?php
+    If (isset($_GET['page'])) {
+      switch ($_GET['page']) {
+        case 'site_layout':
+          include('include/' . 'inc_site_layout.php');
+          break;
+        case 'control_structures':
+          include('include/' . 'inc_control_structures.php');
+          break;
+        case 'string_functions':
+          include('include/' . 'inc_string_functions.php');
+          break;
+        case 'web_forms':
+          include('include/' . 'inc_web_forms.php');
+          break; 
+        case 'midterm_assessment':
+          include('include/' . 'inc_midterm_assessment.php');
+          break;
+        case 'state_information':
+          include('include/' . 'inc_state_information.php');
+          break;
+        case 'user_templates':
+          include('include/' . 'inc_user_templates.php');
+          break;
+        case 'final_project':
+          include('include/' . 'inc_final_project.php');
+          break;
+        case 'home_page': // A value of 'home_page' means to display the default page
+        default:
+          include('include/inc_home.php');
+          break;
+        }
+    } else { 
+      // If no button has been selected, then display the default page
       include('include/inc_home.php');
-      break;
     }
-} else { 
-  // If no button has been selected, then display the default page
-  include('include/inc_home.php');
-}
-?>
+?></p>
 </div>
 
-<div id="footer">
-<?php include_once(__DIR__.'/include/inc_footer.php') ?>
-</div>
-
+  <div id="footer">
+    <p><?php include_once(__DIR__.'/include/inc_footer.php') ?></p>
+  </div>
+</div> <!-- wrapper -->
 </body>
 
 </html>
