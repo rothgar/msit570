c082e85bab09d8ef732887f422a9ac0ea237b5b5
Justin Garrison <justinleegarrison@gmail.com>
2015-01-22 23:57:22 -0800

    discovery project 5-4 with testing

--- ChineseZodiac/include/inc_control_structures.php
+++ ChineseZodiac/include/inc_control_structures.php
@@ -1,12 +1,40 @@
 <a href="#string">String Functions</a>
 <a href="#char">Character Functions</a>
+<a href="#if_else">If...Else Statement</a>
+<a href="#switch">Switch Statement</a>
+<a href="#while_loop">While Loop</a>
+<a href="#for_loop">For Loop</a>
 
-<p><a id="string"><h3>String Functions</h3></a></p>
+<h1>String and Character Functions</h3>
+
+<a id="string"><h3>String Functions</h3></a>
   <p>Find similar strings in each Zodiac sign</p>
   <a href="SimilarNames.php">[Test the Script]</a>
   <a href="ShowSourceCode.php?source_file=SimilarNames.php">[View the Source Code]</a>
   
-<p><a id="char">Character Functions</a></p>
+<a id="char"><h3>Character Functions</h3></a>
   <p>Finds out what signs can be spelled with the letters from the phrase "Embed PHP scripts within an XHTML document."</p>
   <a href="EmbeddedWords.php">[Test the Script]</a>
   <a href="ShowSourceCode.php?source_file=EmbeddedWords.php">[View the Source Code]</a>
+
+<h1>Conditional Statements and Looping Structures</h1>
+
+<a id="if_else"><h3>If...Else Statement</h3></a>
+  <p>Show how an if else statement can be used to conditionally run code.</p>
+  <a href="BirthYear_ifelse.php">[Test the Script]</a>
+  <a href="ShowSourceCode.php?source_file=BirthYear_ifelse.php">[View the Source Code]</a>
+
+<a id="switch"><h3>Switch Statement</h3></a>
+  <p>Use a switch statement to select a section of php code to run.</p>
+  <a href="BirthYear_switch.php">[Test the Script]</a>
+  <a href="ShowSourceCode.php?source_file=BirthYear_switch.php">[View the Source Code]</a>
+
+<a id="while_loop"><h3>While Loop</h3></a>
+  <p>Run code snippet until a specific criteria is met.</p>
+  <a href="Chinese_Zodiac_while_loop.php">[Test the Script]</a>
+  <a href="ShowSourceCode.php?source_file=Chinese_Zodiac_while_loop.php">[View the Source Code]</a>
+
+<a id="for_loop"><h3>For Loop</h3></a>
+  <p>Run code snippet a specific number of times.</p>
+  <a href="Chinese_Zodiac_for_loop.php">[Test the Script]</a>
+  <a href="ShowSourceCode.php?source_file=Chinese_Zodiac_for_loop.php">[View the Source Code]</a>
