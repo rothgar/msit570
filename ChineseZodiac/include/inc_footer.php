cf443e049cf42b7830d0badfd909a7dbff10f905
Justin Garrison <justinleegarrison@gmail.com>
2015-01-23 00:10:03 -0800

    discovery project 5-5 part 2

--- ChineseZodiac/include/inc_footer.php
+++ ChineseZodiac/include/inc_footer.php
@@ -1,1 +1,8 @@
 &copy; 2014
+<?php
+$prov_array = file("proverbs.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$proverbcount = count($prov_array);
+$index = rand(0, $proverbcount-1);
+echo "A randomly displayed Chinese proverb read from a text file<br />";
+echo $prov_array[$index];
+?>
