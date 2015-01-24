a00f68aed3f6bb83f1b780f4905183f0a81223c3
Justin Garrison <justinleegarrison@gmail.com>
2015-01-23 21:54:34 -0800

    added php.ini for file uploads and updated docker run

--- ChineseZodiac/run_docker.sh
+++ ChineseZodiac/run_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm --name zodiac -v "$(pwd)":/var/www/html php:5.6-apache
+docker run -it --rm --name zodiac -v "$(pwd)":/var/www/html -v "$(pwd)"/php.ini:/usr/local/etc/php/php.ini php:5.6-apache
