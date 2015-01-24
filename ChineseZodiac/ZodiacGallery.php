db989e4dcf91c1e7870b3ebd3fd5d0d1e660ee7f
Justin Garrison <justinleegarrison@gmail.com>
2015-01-24 00:33:59 -0800

    6-3 :mask:

---
+++ ChineseZodiac/ZodiacGallery.php
@@ -0,0 +1,22 @@
+<?php
+$signs = array( 
+  "monkey.png" => "monkey",
+  "rooster.png" => "rooster",
+  "dog.png" => "dog",
+  "pig.png" => "pig", 
+  "rat.png" => "rat", 
+  "ox.png" => "ox", 
+  "tiger.png" => "tiger", 
+  "rabbit.png" => "rabbit", 
+  "dragon.png" => "dragon", 
+  "snake.png" => "snake", 
+  "horse.png" => "horse", 
+  "sheep.png" => "sheep"
+);
+
+foreach ( $signs as $image => $caption ) {
+  $size = getimagesize("images/$image");
+  //DEBUGGING print_r($size);
+  echo '<a href="images/'.$image.'"><img src="images/'.$image.'" alt="'.$caption.'" height="75" width="75"></a>';
+}
+?>
